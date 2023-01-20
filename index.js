fa8d668efb5c0caf39e88f59a612cccf922f17a5
Allen Ding <allen.ding@hotmail.com>
2023-01-20 15:43:59 -0800

    uncommented out changes

--- index.js
+++ index.js
@@ -27,22 +27,22 @@ const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocat
 //   console.log(data);
 // });
 
-// nextISSTimesForMyLocation((error, passTimes) => {
-//   if (error) {
-//     return console.log("It didn't work!", error);
-//   }
-//   // success, print out the deets!
-//   printPassTimes(passTimes);
-// });
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
 
 
-// const printPassTimes = function(passTimes) {
-//   for (const pass of passTimes) {
-//     const datetime = new Date(0);
-//     datetime.setUTCSeconds(pass.risetime);
-//     const duration = pass.duration;
-//     console.log(`Next pass at ${datetime} for ${duration} seconds!`);
-//   }
-// };
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
 
 console.log(fetchCustomIP()); 
